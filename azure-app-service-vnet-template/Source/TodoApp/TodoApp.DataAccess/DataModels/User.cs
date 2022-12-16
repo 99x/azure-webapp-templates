35af181fc423f44a0df03e3fe26cc24bafac2bb5
Sachith Samarakoon <56157368+sachiths99xt@users.noreply.github.com>
2022-12-16 11:39:28 +0530

    Add database models and initial migration (#19)
    * add database models and initial migration
    
    * Added User class and made some minor changes
    
    * moved UseSqlServer function to Program.cs
    
    Co-authored-by: Sachith Samarakoon <sachiths@compello.com>
    Co-authored-by: Ganeshmoorthi Dilshan Prasad <dilshanprasadbrown@gmail.com>

---
+++ azure-app-service-vnet-template/Source/TodoApp/TodoApp.DataAccess/DataModels/User.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApp.DataAccess.DataModels
+{
+    [Table("User")]
+    public class User
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+
+    }
+}
